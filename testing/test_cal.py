515c740f0f725aa11f55393f8cb0072c3338028a
zhangniuniu1234 <n1997z@163.com>
2020-05-13 18:26:09 +0800

    提交

--- testing/test_cal.py
+++ testing/test_cal.py
@@ -1,6 +1,16 @@
+import allure
 import pytest
 from testpy.cal import  Calc
 import  yaml
+
+
+#加载测试步骤yml文件
+def load_step():
+    with open("data/test_step.yml") as f:
+        steps = f.read()
+        print(steps)
+        return steps
+
 
 
 #判断两个浮点数是否相等
@@ -18,35 +28,47 @@ def compare_float(a, b, precision):
     return False
 
 
+@allure.feature("测试用例类")
 class TestCalc:
 
 
     @pytest.mark.parametrize('a,b,exp',
                              yaml.safe_load(open("data/add.yml")))
     @pytest.mark.add
+    @allure.step("测试加法函数")
     def calc_add(self,fixture_setup, a, b, exp):
-        if isinstance(a,(int,float) ) is not True:
-            raise TypeError("传入的参数不是整数")
+        steps=load_step()
+        for step in  steps:
+            if 'add' in step:
+                if (isinstance(a, (int, float)) is not True) or isinstance(b, (int, float)) is not True :
+                    # pytest.skip("不支持非整数的参数")
+                    raise TypeError("传入的参数不是整数")
 
-        result=fixture_setup.add(a,b)
-        print(result)
-        if compare_float(result,exp,0.00001):
-            exp = result
-        assert result == exp
+                result = fixture_setup.add(a, b)
+                print(result)
+                if compare_float(result, exp, 0.00001):
+                    exp = result
+                assert result == exp
+
 
 
     @pytest.mark.parametrize('a,b,exp',
                              yaml.safe_load(open("data/div.yml")))
     @pytest.mark.div
+    @allure.step("测试除法函数")
     def calc_div(self,fixture_setup, a, b, exp):
-        try:
-            result = fixture_setup.div(a, b)
-            if compare_float(result, exp, 0.00001):
-                exp = result
-        except ZeroDivisionError:
-            print("0不能做除数")
-        else:
-            assert result == exp
+        steps=load_step()
+        for step in steps:
+            if 'div' in step:
+                try:
+                    result = fixture_setup.div(a, b)
+                    if compare_float(result, exp, 0.00001):
+                        exp = result
+                except ZeroDivisionError:
+                    print("0不能做除数")
+                else:
+                    assert result == exp
+
 
 
     @pytest.mark.parametrize('a,b,exp',
@@ -72,7 +94,9 @@ class TestCalc:
 
 
 if __name__=="__main__":
+    # load_step()
     pytest.main(["-v","-s","-m add or div"])
+
 
 
 
